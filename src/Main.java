2a0abc5c8ae0c92f68b26fd5763dd24c64a2bde7
Rediaj04 <j.a.cabarcas07@gmail.com>
2024-04-23 19:18:02 +0200

    Reacomodamiento del codigo en general

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,35 @@
 import java.util.Scanner;
 
-public class Main extends TUI {
+public class Main {
+    public static void main(String[] args) {
+        TUI tui = new TUI();
+        int x;
+
+        do {
+            tui.Menu("Elige un número de las opciones a escoger:");
+            tui.NuevaPartida("1. Nueva partida");
+            tui.CargarPartida("2. Cargar partida");
+            tui.Configuraciones("3. Configuración");
+            tui.Salir("4. Salir");
+
+            x = tui.OpcionElegida();
+
+            switch (x) {
+                case 1:
+                    System.out.println("Nueva partida");
+                    break;
+                case 2:
+                    System.out.println("Cargar partida");
+                    break;
+                case 3:
+                    System.out.println("Configuración");
+                    break;
+                case 4:
+                    System.out.println("Salir");
+                    break;
+                default:
+                    System.out.println("Opción incorrecta");
+            }
+        } while (x < 1 || x > 4);
+    }
 }

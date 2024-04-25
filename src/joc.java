27a496ba5c37daaaa1394ed6d37d7afc7488a79f
daw1 <dcano@elpuig.xeill.net>
2024-04-25 20:06:58 +0200

    He añadido métodos en joc y he llamado a la clase joc desde el main

--- src/joc.java
+++ src/joc.java
@@ -1,15 +1,13 @@
 import jdk.jshell.spi.ExecutionControl;
 
 public class joc {
-    public char taulell {
-        return void;
-    }
+    public char[][] taulell;
 
-    public void novaPartida(){
-        throw new ExecutionControl.NotImplementedException();
+    public void novaPartida() throws ExecutionControl.NotImplementedException {
+        throw new ExecutionControl.NotImplementedException("");
     }
-    public void jugar(short fila, String columna){
-        throw new ExecutionControl.NotImplementedException();
+    public void jugar(short fila, String columna) throws ExecutionControl.NotImplementedException {
+        throw new ExecutionControl.NotImplementedException("");
     }
     public void jugadaGuanyadora(short fila, String columna){
      boolean x;
